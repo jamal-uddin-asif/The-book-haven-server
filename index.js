2b52ce25dfd77226f28f7cea7e6c30debd433fff
jamal-uddin-asif <asifzehendmg@gmail.com>
2025-11-11 19:11:39 +0600

    Latest books APIs created

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const uri =
+  "mongodb+srv://booksDB:Lq3y3xiJIALEYJhG@cluster0.ltnfqwl.mongodb.net/?appName=Cluster0";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Book Haven running fine");
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const booksDB = client.db('BooksDB')
+    const booksCollection = booksDB.collection('books')
+
+
+    app.get('/latest-books', async(req, res)=>{
+        const cursor = booksCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Book haven listening on port ${port}`);
+});
